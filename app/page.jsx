03c72e61a68cdbd7234ee6fbcdab29d89eb95381
tachyonok <tachyonparticle123@gmail.com>
2024-06-22 16:01:26 +0530

    tech rent lfg

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import "@/assets/styles/globals.css";
+
+export const metadata = {
+  title: "Tachrent | find your perfect Rental",
+  description: "Tachrent - Your one-stop-shop for all your rental needs",
+  keywords: "rental , find rentals , find properties",
+};
+
+const HomePage = () => {
+  return <div>HomePage</div>;
+};
+
+export default HomePage;
